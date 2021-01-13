85224abc160424c0c271ee754460ada3f66f2a82
Jens <jens@jensz12.com>
2021-01-13 13:03:01 +0100

    inital commit
    Still needs stuff done

---
+++ source/TechFlag.cs
@@ -0,0 +1,25 @@
+﻿using ICities;
+
+namespace TechFlag
+{
+    public class KajsTechFlag : IUserMod
+    {
+        public string Name
+        {
+            get { return "Kajs Tech Support flag replacer"; }
+        }
+
+        public string Description
+        {
+            get { return "Replaces the stock flags with the Kajs Tech Support Flag"; }
+        }
+
+        public string[][] CustomFlags()
+        {
+            return new[]
+            {
+                new[] {"kajstech", "Kajs Tech Support"},
+            };
+        }
+    }
+}
